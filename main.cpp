756c30d5f18743be7f4cb1fc8e01f6dd997c16ea
Paul Meffle <meffle.paul@me.com>
2017-06-19 22:37:56 +0200

    Extend example

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 
 int main()
 {
+        LOG.setMinSeverity(Log::WARNING);
+
         LOG << Log::DEBUG << "Test";
         LOG.e() << "ERROR";
+        Log::logger.w() << "Count should not be " << 4;
 
         return 0;
 }
