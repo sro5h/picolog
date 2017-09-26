b9ee90992a108e00ffc7463df2c2ff0bb9823f80
Paul Meffle <meffle.paul@me.com>
2017-09-26 17:01:26 +0200

    Increment version number

--- picolog.hpp
+++ picolog.hpp
@@ -1,5 +1,5 @@
 /*
-   picolog.hpp - 1.0.2
+   picolog.hpp - 1.1.0
 
    Author:
         Paul Meffle
@@ -12,6 +12,7 @@
         1.0   (14.08.2017) initial release
         1.0.1 (12.09.2017) remove summary
         1.0.2 (13.09.2017) add documentation
+        1.1.0 (26.09.2017) use operator() to set severity
 */
 
 #ifndef _PICOLOG_HPP
